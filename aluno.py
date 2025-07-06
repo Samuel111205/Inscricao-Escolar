b22400a337114d812c5c7cb562d699f492c05000
Samuel111205 <samuelkundinho@gmail.com>
2025-07-06 13:23:07 +0100

    novas funcionalidades
    melhorei a escolha e o gerenciamento de cadastro

--- aluno.py
+++ aluno.py
@@ -1,51 +1,59 @@
-import cadastro
+import cadastro 
 from datetime import date
 
 while True:
     cadastro.menu()
     opcao=int(input("Digite sua opção: "))
     if opcao==1:
-        nome=input("Nome: ")
-        nascimento=int(input("Data de nascimento: "))
-        idade=date.today().year-nascimento
-        media=float(input("Media do certificado: "))
-        curso=input("Digite seu curso: ")
-        aluno=cadastro.Aluno(nome,nascimento,idade,media,curso)
-        if idade>=15 and idade<=18 and media>=10:
-            cadastro.cad.append(aluno)
-            print("Aluno cadastrado com sucesso")
-        else:
-            cadastro.nao_cadastradas.append(aluno)
-            print("Aluno não cadastrado")
-    elif opcao==2:
-        if cadastro.cad:
-            for i in cadastro.cad:
-                print(f"{i}: {i.mostrar_dados()}")
-        else:
-            print("Nenhum aluno cadastrado")
-    elif opcao==3:
-        for p in cadastro.nao_cadastradas:
-            print(f"{p}: {p.mostrar_dados()}")
-    elif opcao==4:
-        nome=input("Nome: ")
-        pagamento=input("Pagamento: ").capitalize()
-        while True:
-            if pagamento=="Sim":
-                idade=int(input("Idade: "))
-                classe=int(input("Classe: "))
-                curso=input("Curso: ")
-                matricula=cadastro.Matricula(nome,pagamento,idade,classe,curso)
-                matricula.mostrar_dados()
-                break
-            elif pagamento=="Nao":
-                print("Para continuares o processo de maricula tens de fazer o pagamento do rupe")
-                print("dirija-se a secretaria do ceu curso  e receba um rupe")
-                break
+        cadastro.menu_cadastro()
+        opp=int(input("Digite o operação a ser feita: "))
+        if opp==1:
+            nome=input("Nome: ")
+            nascimento=int(input("Data de nascimento: "))
+            idade=date.today().year-nascimento
+            media=float(input("Media do certificado: "))
+            curso=input("Digite seu curso: ")
+            aluno=cadastro.Aluno(nome,nascimento,idade,media,curso)
+            if idade>=15 and idade<=18 and media>=10:
+                cadastro.cadastrados.append(aluno)
+                print("Aluno cadastrado com sucesso")
             else:
-                print("Resposta errada por favor digite apenas Sim ou Nao")
-                break
-    elif opcao==5:
-        break
-    else:
-        print("Opçaõ invalida!")
+                cadastro.nao_cadastradas.append(aluno)
+                print("Aluno não cadastrado")
+            cadastro.total_inscritos.append(aluno)
+        elif opcao==2:
+            if cadastro.cadastrados:
+                for i in cadastro.cadastrados:
+                    print(f"{i}: {i.mostrar_dados()}")
+            else:
+                print("Nenhum aluno cadastrado")
+        elif opcao==3:
+            for p in cadastro.nao_cadastradas:
+                print(f"{p}: {p.mostrar_dados()}")
+        elif opcao==4:
+            nome=input("Nome: ")
+            pagamento=input("Pagamento: ").capitalize()
+            while True:
+                if pagamento=="Sim":
+                    idade=int(input("Idade: "))
+                    classe=int(input("Classe: "))
+                    curso=input("Curso: ")
+                    matricula=cadastro.Matricula(nome,pagamento,idade,classe,curso)
+                    matricula.mostrar_dados()
+                    break
+                elif pagamento=="Nao":
+                    print("Para continuares o processo de maricula tens de fazer o pagamento do rupe")
+                    print("dirija-se a secretaria do ceu curso  e receba um rupe")
+                    break
+                else:
+                    print("Resposta errada por favor digite apenas Sim ou Nao")
+                    break
+        elif opcao==5:
+            print(f"Ao todo foram {len(cadastro.total_inscritos)} alnos inscritos", end=" ")
+            print(f"Na qual {len(cadastro.cadastrados)} alunos foram cadastrados comsucesso",end=" ")
+            print(f"E {len(cadastro.nao_cadastradas)} não foram cadastradas")
+        elif opp==6:
+            break
+        else:
+            print("Opçaõ invalida!")
 
--- cadastro.py
+++ cadastro.py
@@ -1,3 +1,4 @@
+#class Mãe 
 class Pessoa:
     def __init__(self,nome,nascimento,idade,media):
         self.nome=nome
@@ -8,6 +9,7 @@ class Pessoa:
         print("Mostrar os dados do Aluno! ")
 
 
+#subclasse aluno que herda da classe mãe pessoa 
 class Aluno(Pessoa):
     def __init__(self,nome,nascimento,idade,media,curso):
         super().__init__(nome,nascimento,idade, media)
@@ -19,6 +21,7 @@ class Aluno(Pessoa):
         print(f"Curso: {self.curso}")
 
 
+#sublcasse matricula que herda da classe mãe pessoa
 class Matricula(Pessoa):
     def __init__(self, nome, nascimento, idade,classe,media):
         super().__init__(nome, nascimento, idade,media,)
@@ -29,15 +32,21 @@ class Matricula(Pessoa):
         print(f"Classe: {self.classe}ªClasse")
         print(f"Curso: {self.media}")
     
-
+#função que exibe o menu de opções
 def menu():
     print("===SISTEMA DE INSCRIÇÃO===")
+    print("1.Gerenciamentos de cadastro")
+   
+
+def menu_cadastro():
     print("1.Cadastrar alunos")
     print("2.Listar alunos cadastrados")
     print("3.Lisatr alunos não cadastrados")
     print("4.Fazer Matricula")
     print("5.Calcular total de inscrito")
-    print("5.Sair")
+    print("6.Sair")
 
-cad=[]
-nao_cadastradas=[]
+
+cadastrados=[]#lista que armazena os alunos cadastrados
+nao_cadastradas=[]#lista que armazena os alunos não cadastradas 
+total_inscritos=[]#lista que armazena o numero total de pessoas incritas
